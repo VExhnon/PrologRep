54a70524f8221900869e87069bc971b09dd0a868
VExhnon <62421356+VExhnon@users.noreply.github.com>
2021-02-17 16:21:10 +0300

    2 and 3 tasks

--- lab3/base.pl
+++ lab3/base.pl
@@ -1,5 +1,7 @@
 max(X,Y,Z):-X>Y,Z is X,write(Z);X<Y,Z is Y,write(Z).
 
+max(X,Y,U,Z):-X>Y,X>U,Z is X,write(Z);Y>X,Y>U,Z is Y,write(Z);U>X,U>Y,Z is U,write(Z).
 
-
+factorial(0,1):-!.
+factorial(X,Y):-Z is X-1,factorial(Z,W),Y is X*W.
 
