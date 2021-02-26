f83fcaa376942dc946e131a072f536232465b7fd
§0DENCHIK §4yout <ilia.gagaga@mail.ru>
2021-02-27 00:27:20 +0300

    2 task

--- lab4/base.pl
+++ lab4/base.pl
@@ -7,3 +7,10 @@ read_list(N,CurN1,CurList1,L).
 
 write_list([]):-!.
 write_list([H/T]):-write(H),write_list(T).
+
+
+list_sum([], 0).
+%list_sum([Head|_],S):-S is Head.
+list_sum([Head|Tail],S):-list_sum(Tail,S1),S is (S1+Head).
+
+
